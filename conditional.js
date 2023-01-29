92f4aa276a0c5a728f6217a0ba474a44dd6e88c4
barkatzx <barkatullah.zx@gmail.com>
2023-01-30 00:29:37 +0300

    Condition Concept

---
+++ conditional.js
@@ -0,0 +1,22 @@
+var iphonePrice = 90000;
+var myBudget = 150000;
+// If iPhone Price is Less Than My Budget. I will buy The iPhone.
+if (iphonePrice < myBudget){
+    console.log('Happiest Moment');
+}
+
+var chickenPrice = 500;
+var myMoney = 50;
+if (chickenPrice < myMoney){
+    console.log('I eat Chicken');
+}
+if (chickenPrice > myMoney){
+    console.log('I eat Cha Ruti')
+}
+
+if (chickenPrice < myMoney){
+    console.log('I will eat chicken');
+}
+else{
+    console.log('I will eat potato');
+}
---
+++ multiple-condition.js
@@ -0,0 +1,8 @@
+var isGraduated = true;
+var salary = 25000;
+if(isGraduated === true && salary > 50000){
+    console.log('Eso Biye kore Feli');
+}
+else{
+    console.log('Kopale Biya Nai');
+}
