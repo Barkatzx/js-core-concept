8971ab8acbfff9e5ce423372064ff2d784927553
barkatzx <barkatullah.zx@gmail.com>
2023-01-30 00:03:07 +0300

    Comparison Concept

---
+++ comparison.js
@@ -0,0 +1,20 @@
+console.log(5 < 6);
+console.log(5 > 6);
+console.log(5 == 6);
+console.log(5 !== 6);
+console.log(15 !== 15);
+// Less Than Or Equal
+console.log(5 <= 6);
+console.log(6 <= 6);
+console.log(16 <= 6);
+// Greater Than Or Equal
+console.log(5 >= 6);
+console.log(5 >= 2);
+console.log(2 >= 2);
+
+// Comparison With Variable
+var number1 = 56;
+var number2 = 89;
+
+console.log(number1 < number2);
+console.log(number1 > number2);
